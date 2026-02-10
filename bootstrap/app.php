62fbe834cea62a2884be89a95797edcbe5aa6aad
rannyzita <fs.vitoria.soares@gmail.com>
2026-02-10 15:05:19 -0300

    build: configura o backend com o inertia.js, para facilicitar a interação e rodar tudo no blade

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,14 +5,19 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+->withRouting(
+    web: __DIR__.'/../routes/web.php',
+    commands: __DIR__.'/../routes/console.php',
+    health: '/up',
     )
+    //conecta o Inertia com o Laravel, para que o Inertia possa lidar com as requisições e respostas do Laravel, por isso, usamos esse middleware para registrar o HandleInertiaRequests, que é responsável por lidar com as requisições do Inertia e enviar as respostas corretas para o cliente.
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+    // o folder bootstrap é responsável por configurar o ambiente de execução do Laravel, como carregar as dependências, configurar o autoloading, registrar os providers e middlewares, entre outras coisas. O arquivo app.php é o ponto de entrada para a aplicação, onde a instância do Application é criada e configurada. Ele também é responsável por registrar as rotas da aplicação, os middlewares e as exceções.
