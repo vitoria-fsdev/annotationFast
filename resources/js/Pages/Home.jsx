9befcf2c14c0af192654bb420b7e1e6550a0272b
rannyzita <fs.vitoria.soares@gmail.com>
2026-02-10 14:56:37 -0300

    build: configura o blade para enxergar o react

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home () {
+    return (
+        <div className="div_center" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,7 @@
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './Pages/Home';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<Home />);
