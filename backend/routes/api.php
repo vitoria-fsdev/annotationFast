5176925b69d11e75fc5edb8b4c1e162df965464c
rannyzita <fs.vitoria.soares@gmail.com>
2026-02-20 17:36:13 -0300

    chore: refactor api routes to use apiResource

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,4 +4,6 @@ use App\Http\Controllers\AnnotationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/annotations', [AnnotationController::class, 'index']);
+// o nome ta como annotations.index, annotations.store, annotations.show, annotations.update e annotations.destroy, 
+// caso queira reverenciar as rotas em algum controller so usar o name da rota, exemplo: route('annotations.index') para acessar a rota de listagem de anotações
+Route::apiResource('annotations', AnnotationController::class);
